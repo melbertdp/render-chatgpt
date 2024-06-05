1ae670ed980446b7ce6cae9f2efb909c96781aac
melbertdp <110758934+melbertdp@users.noreply.github.com>
2024-06-05 13:24:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:8092, https://benchmarking-test2.accenture.com");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   next();
 });
 
